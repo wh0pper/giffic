37b0a7bb43c6c867a45c27cd3a3598a3351e4863
jared clemmensen and roger yee <roger@>
2018-04-18 10:24:13 -0700

    adds bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+node_list = [
+    [ "Once upon a time ", "rj", "https://media.giphy.com/media/xSlBkf2Yk1uV2/giphy.gif" ]
+]
+
+node_list.each do |sentence, signature, gif_url|
+  Node.create( sentence: sentence, signature: signature, gif_url: gif_url )
+end
---
+++ spec/controllers/nodes_controller_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe NodesControllerController, type: :controller do
+
+end
