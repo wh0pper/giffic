733afd5b2b6d9470d41340f838d51e44a31b8686
jared clemmensen and roger yee <roger@>
2018-04-19 12:00:04 -0700

    discover bug in new node assignment

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -5,13 +5,15 @@ class NodesController < ApplicationController
     new_node_params = params[:node]
     new_node = Node.create(sentence: new_node_params[:sentence], signature: new_node_params[:signature], gif_url:   new_node_params[:gif_url])
     @story.nodes.push(new_node)
+    redirect_to :controller => 'stories_controller.rb', :action => 'show'
   end
 
   def update
-    @story = Story.first
+    @story = Story.find(params[:story_id])
     selected_node = Node.find(params[:id])
     @story.nodes.push(selected_node)
     # @next_nodes = Node.last(2)
     @previous_nodes = @story.nodes
+    redirect_to :controller => 'stories', :action => 'show', :id => @story.id
   end
 end
